7b6fe7ecacc684590a8c260257db41bd02c570c5
Jeff Patton <jeffrey@patton-tech.com>
2022-01-28 05:02:19 +0000

    Added some descriptions to make the docs look nice

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "exports" {
   value       = azurerm_automation_account.automation_account
+  description = "Return the Automation Account details."
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "ResourceGroupName" {
   type        = string
+  description = "Name of the ResourceGroup to deploy into."
 }
 
 variable "name" {
   type        = string
+  description = "Name of the Automation Account."
 }
 
 variable "tags" {
  type         = object
+ description = "Tags to add to the Automation Account."
 }
