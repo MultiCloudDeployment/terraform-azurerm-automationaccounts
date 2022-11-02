dc3c1e8f148714e38c354ad29f57dba3c5879dd8
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-02 00:05:21 -0500

    Updated template with additional parameters, and corrected var file

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.93.1"
+      version = "3.29.1"
     }
   }
 }
@@ -12,14 +12,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_automation_account" "automation-account" {
-  name                  = var.name
-  location              = var.location
-  resource_group_name   = var.resourceGroup
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resourceGroup
 
-  sku_name              = var.skuName
+  sku_name                      = var.skuName
   
-  disable_local_auth    = var.disableLocalAuth
-  public_network_access = var.publicNetworkAccess
+  local_authentication_enabled  = var.disableLocalAuth
+  public_network_access_enabled = var.publicNetworkAccess
 
   tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,33 @@
-variable "ResourceGroupName" {
+variable "name" {
+  type        = string
+  description = "Name of the Automation Account."
+}
+
+variable "location" {
+  type        = string
+  description = "Region to deploy into."
+}
+
+variable "resourceGroup" {
   type        = string
   description = "Name of the ResourceGroup to deploy into."
 }
 
-variable "name" {
+variable "skuName" {
   type        = string
-  description = "Name of the Automation Account."
+  default     = "Basic"
+  description = "The SKU of the account."
+}
+
+variable "disableLocalAuth" {
+  type        = bool
+  description = "Whether requests using non-AAD authentication are blocked."
+}
+
+variable "publicNetworkAccess" {
+  type        = bool
+  default     = true
+  description = "Whether public network access is allowed for the container registry."
 }
 
 variable "tags" {
